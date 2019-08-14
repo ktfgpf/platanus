40ec63a8bb7ccae22649d50c3993c65c055b77f7
tkhr <taka_gergery21@yahoo.co.jp>
2019-08-14 18:07:46 +0900

    comment

--- platanus/settings.py
+++ platanus/settings.py
@@ -74,11 +74,10 @@ WSGI_APPLICATION = 'platanus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url
+db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
